7b1bbf77cfed5ca0d1df3342842d13e1b16dcfc5
Nick Alto <nickalto@gmail.com>
2011-10-03 21:00:17 -0700

    Basically Finished Final Clean Up
    * Clean up code and comments
    * Use time utility for write up
    * Fix print function
    * Make file
    
    Questions for TA:
    *srand48 not working for me - hard coded okay?
    *My implementation threads>dimensions is useless will I get graded down
    if I change it threads=dimensions
    *Print product of two matrixes? or Identity matrix? All Matrixes?
    *Graded down for including structs in multiple places?

--- pMult.c
+++ pMult.c
@@ -10,6 +10,7 @@ Oregon State University
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 /* Global Variables */
 int dimension;      /*user provided dimension of matrix e.g. nxn*/
@@ -25,15 +26,112 @@ void *matMult(void *abc) {
   typedef struct {
     float matrix1[dimension][dimension];
     float matrix2[dimension][dimension];
-    int matrixResult[dimension][dimension];
+    float matrixResult[dimension][dimension];
     int numOfThreads;
+    int prevCalc;
 } data;
 
 data* sysData= (data*)abc;
+int threadCalc;
+int leftOvers;
+int i;
+int x;
+int y;
+int mtxCount;
+float result;
+  threadCalc  = floor((double)(dimension*dimension)/(double)sysData->numOfThreads);
+leftOvers = dimension %sysData->numOfThreads; 
 
+ for(i = 0; i<leftOvers; i++)
+ {
+   if(threadID == i)
+   {
+     threadCalc++;
+   }
+ }
+
+ for(i=0; i< threadCalc; i++)
+ { 
+  x = floor((double)sysData->prevCalc / (double)dimension);
+ y = sysData->prevCalc % dimension;
+    for(mtxCount = 0; mtxCount < dimension; mtxCount++)
+    {
+      result += (sysData->matrix1[x][mtxCount] * sysData->matrix2[mtxCount][y]);
+      /*printf("\nmatrix 1: %f  *  matrix 2: %f\n", sysData->matrix1[x][mtxCount], sysData->matrix2[mtxCount][y]);*/
+    }
+    /*printf("result = %f\n\n", result);*/
+    sysData->matrixResult[x][y] = result;
+    sysData->prevCalc ++;
+    result = 0;
+
+ } 
+
+
+/*printf("\n Thread %d   work: %d\n   previous work = %d  x: %d  y: %d\n", threadID,threadCalc, sysData->prevCalc,x,y);
 printf("working up here %f for thread %d\n",sysData->matrix1[0][0], threadID);
+*/
 threadID++;
 pthread_exit(NULL);
+}/*end function matMult()*/
+
+
+
+ void *printData(void *arg, int k){
+int i;
+int j;
+
+     typedef struct {
+    float matrix1[dimension][dimension];
+    float matrix2[dimension][dimension];
+    float matrixResult[dimension][dimension];
+    int numOfThreads;
+    int prevCalc;
+} data;
+
+data* sysData= (data*)arg;
+
+
+for(i=0; i< dimension; i++)
+{
+  printf("[");
+  for(j=0; j<dimension; j++)
+  {
+    if(k == 1)
+    {
+      if((j+1) < dimension)
+      {
+      printf(" %f, ", sysData->matrix1[i][j]);
+      }
+      else
+        printf(" %f ", sysData->matrix1[i][j]);
+
+    }
+    else if(k==2)
+    {
+      if((j+1) < dimension)
+      {
+      printf(" %f, ", sysData->matrix2[i][j]);
+      } 
+      else
+        printf(" %f ", sysData->matrix2[i][j]);
+
+    }
+    else if(k==3)
+    {
+      if((j+1) < dimension)
+      {
+      printf(" %f, ", sysData->matrixResult[i][j]);
+      }
+      else
+       printf(" %f ", sysData->matrixResult[i][j]);
+
+    }
+    
+  }
+  printf("]\n");
+
+}
+return 0;
 }
 
 
@@ -61,14 +159,19 @@ if(argc < 4)
   numThreads = atoi(argv[2]);
   printOut = atoi(argv[3]);
 
+if(numThreads < 1)
+{
+  numThreads = 1;
+}
 /* data struct declared here because the matrixes dimensions are specified
  * by the user and cannot be declared until the proper dimensions are provided.
  */
   typedef struct {
     float matrix1[dimension][dimension];
     float matrix2[dimension][dimension];
-    int matrixResult[dimension][dimension];
+    float matrixResult[dimension][dimension];
     int numOfThreads;
+    int prevCalc;
 } data;
 
 
@@ -92,27 +195,49 @@ printf("%f\n",sysData->matrix1[0][0]);
 
 
 sysData->numOfThreads = numThreads;
+sysData->prevCalc=0;
+ if(numThreads > dimension)
+ {
+   numThreads = dimension;
+   sysData->numOfThreads = dimension;
+ }
 pthread_t tid;
 pthread_attr_t attr;
 
 pthread_attr_init(&attr);
 pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 
+
+/* Create threads and wait for them to finish and rejoin before
+ * doing anything. If no threads are specified call matMult() explicitly.
+ */
+
 for (j=0; j<numThreads; j++) {
   rc = pthread_create(&tid, &attr, matMult, sysData);
   if (rc) {
-    printf("ERROR; return code from pthread_create() is %d\n", rc);
+    printf("ERROR: pthread_create(): is %d\n", rc);
     exit(-1);
     }
 
   /* Wait for the thread */
   rc = pthread_join(tid, NULL);
   if (rc) {
-    printf("ERROR; return code from pthread_join() is %d\n", rc);
+    printf("ERROR: pthread_join(): %d\n", rc);
     exit(-1);
     }
   }
+  /* Default case where number of threads specified == 0 */
+   if(numThreads == 0)
+   {
+     matMult(sysData);
+   }
 
+printf("\nMATRIX 1\n");
+printData(sysData, 1);
+printf("\nMATRIX 2\n");
+printData(sysData, 2);
+printf("\nRESULT\n");
+printData(sysData, 3);
 pthread_attr_destroy(&attr);
 pthread_exit(NULL);
 
